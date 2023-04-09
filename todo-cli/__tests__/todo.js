b10d06aa6fcb290b7a6c1483b8e9ce597ef5998e
yeshwanth1524 <127530128+yeshwanth1524@users.noreply.github.com>
2023-04-09 22:52:28 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -3,6 +3,10 @@ const todoList = require('../todo')
 
 describe('TodoList Test Suite', () => {
   let todos
+  const today = new Date()
+  const oneDay = 60 * 60 * 24 * 1000
+  const yesterday = new Date(today.getTime() - 1 * oneDay)
+  const tomorrow = new Date(today.getTime() + 1 * oneDay)
 
   beforeEach(() => {
     todos = todoList()
@@ -10,12 +14,12 @@ describe('TodoList Test Suite', () => {
 
   test('Should add new todo', () => {
     const initialCount = todos.all.length
-    todos.add({ title: 'New Todo', dueDate: '2023-04-09', completed: false })
+    todos.add({ title: 'New Todo', dueDate: tomorrow.toISOString().slice(0, 10), completed: false })
     expect(todos.all.length).toBe(initialCount + 1)
   })
 
   test('Should mark a todo as completed', () => {
-    const todo = { title: 'New Todo', dueDate: '2023-04-09', completed: false }
+    const todo = { title: 'New Todo', dueDate: tomorrow.toISOString().slice(0, 10), completed: false }
     todos.add(todo)
     const index = todos.all.findIndex(item => item === todo)
     todos.markAsComplete(index)
@@ -23,34 +27,27 @@ describe('TodoList Test Suite', () => {
   })
 
   test('Should retrieve overdue items', () => {
-    todos.add({ title: 'Overdue Todo', dueDate: '2023-04-08', completed: false })
-    todos.add({ title: 'Due Today Todo', dueDate: '2023-04-09', completed: false })
-    const overdue = todos.overdue()
-    expect(overdue.length).toBe(1)
-    expect(overdue[0].title).toBe('Overdue Todo')
+    todos.add({ title: 'Overdue Todo', dueDate: yesterday.toISOString().slice(0, 10), completed: false })
+    todos.add({ title: 'Due Today Todo', dueDate: today.toISOString().slice(0, 10), completed: false })
+    const overdueCount = todos.overdue().length
+    todos.add({ title: 'Overdue Todo 2', dueDate: yesterday.toISOString().slice(0, 10), completed: false })
+    expect(todos.overdue().length).toBe(overdueCount + 1)
   })
 
   test('Should retrieve due today items', () => {
-    todos.add({ title: 'Overdue Todo', dueDate: '2023-04-08', completed: false })
-    todos.add({ title: 'Due Today Todo', dueDate: '2023-04-09', completed: false })
-    const dueToday = todos.dueToday()
-    expect(dueToday.length).toBe(1)
-    expect(dueToday[0].title).toBe('Due Today Todo')
+    todos.add({ title: 'Overdue Todo', dueDate: yesterday.toISOString().slice(0, 10), completed: false })
+    todos.add({ title: 'Due Today Todo', dueDate: today.toISOString().slice(0, 10), completed: false })
+    const dueTodayCount = todos.dueToday().length
+    todos.add({ title: 'Due Today Todo 2', dueDate: today.toISOString().slice(0, 10), completed: false })
+    expect(todos.dueToday().length).toBe(dueTodayCount + 1)
   })
 
   test('Should retrieve due later items', () => {
-    todos.add({ title: 'Overdue Todo', dueDate: '2023-04-08', completed: false })
-    todos.add({ title: 'Due Today Todo', dueDate: '2023-04-09', completed: false })
-    todos.add({ title: 'Due Later Todo', dueDate: '2023-04-10', completed: false })
-    const dueLater = todos.dueLater()
-    expect(dueLater.length).toBe(1)
-    expect(dueLater[0].title).toBe('Due Later Todo')
-  })
-
-  test('Should format todos into a displayable list', () => {
-    todos.add({ title: 'Todo 1', dueDate: '2023-04-08', completed: false })
-    todos.add({ title: 'Todo 2', dueDate: '2023-04-09', completed: true })
-    const displayableList = todos.toDisplayableList(todos.all)
-    expect(displayableList).toBe('[ ] Todo 1 2023-04-08\n[x] Todo 2 ')
+    todos.add({ title: 'Overdue Todo', dueDate: yesterday.toISOString().slice(0, 10), completed: false })
+    todos.add({ title: 'Due Today Todo', dueDate: today.toISOString().slice(0, 10), completed: false })
+    todos.add({ title: 'Due Later Todo', dueDate: tomorrow.toISOString().slice(0, 10), completed: false })
+    const dueLaterCount = todos.dueLater().length
+    todos.add({ title: 'Due Later Todo 2', dueDate: tomorrow.toISOString().slice(0, 10), completed: false })
+    expect(todos.dueLater().length).toBe(dueLaterCount + 1)
   })
 })
