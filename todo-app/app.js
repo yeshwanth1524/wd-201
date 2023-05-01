8c6f1f9d06872a5474ebcbae405cb7a1712039a6
yeshwanth1524 <127530128+yeshwanth1524@users.noreply.github.com>
2023-05-01 20:11:25 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,7 +2,25 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+
+app.get("/", async (request, response) => {
+  const allTodos = await Todo.getTodos();
+  if (request.accepts("html")) {
+    response.render("index", {
+      allTodos,
+    });
+  } else {
+    response.json({
+      allTodos,
+    });
+  }
+});
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (request, response) {
   response.send("Hello World");
