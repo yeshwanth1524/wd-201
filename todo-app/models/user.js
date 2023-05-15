17bf7756c7e55e120357611d64bfab64097a591a
yeshwanth1524 <127530128+yeshwanth1524@users.noreply.github.com>
2023-05-15 22:32:40 +0530

    Add files via upload

---
+++ todo-app/models/user.js
@@ -0,0 +1,48 @@
+'use strict';
+const {Model} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasMany(models.Todo, {
+        foreignKey: 'userId'
+      })
+      // define association here
+    }
+  }
+  User.init({ 
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+      },
+    },
+  // lastname can be empty
+  lastName: DataTypes.STRING,
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: true,
+    },
+  },
+   
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
